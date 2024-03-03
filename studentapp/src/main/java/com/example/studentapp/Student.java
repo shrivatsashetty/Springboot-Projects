bb292a8b24fb5e4d43622f2e434a1a8933e8a2a2
shrivatsashetty <shrivatsa307@gmail.com>
2024-03-03 22:50:55 +0530

    completed first version of student app using spring

--- studentapp/src/main/java/com/example/studentapp/Student.java
+++ studentapp/src/main/java/com/example/studentapp/Student.java
@@ -2,12 +2,25 @@ package com.example.studentapp;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+/* the dependent class */
 public class Student {
 	
 	/* attributes */
+	@Value("Shrivatsa Shetty")
+	private String studentName;
+	@Value("1")
 	private int stdId;
+	@Value("10")
 	private int grade;
+	
+	// Can't set default value to dob of type Date and school of type School since both are objects 
 	private Date dob;
+	
+	//@Autowired // injects the dependency at the runtime but waste since I can't access the attribute outside the class since it's provate
+	private School school;
 	
 	/* getter and setters */
 	public int getStdId() {
@@ -32,6 +45,29 @@ public class Student {
 
 	public void setGrade(int grade) {
 		this.grade = grade;
+	}
+	
+	public School getSchool() {
+		return school;
+	}
+	
+	@Autowired // auto injects the required School dependency
+	public void setSchool(School school) {
+		this.school = school;
+	}
+	
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public void initStudent() {
+		System.out.println("Init method called...\nInitialized a new object of class Student");
+		this.dob = new Date();
+		System.out.println("Date of object creation: " + this.dob);
 	}
 	
 }
