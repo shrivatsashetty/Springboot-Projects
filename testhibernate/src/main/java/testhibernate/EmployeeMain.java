434d469630d27286e158ff66265de49104bb8c5d
shrivatsashetty <shrivatsa307@gmail.com>
2024-03-08 07:28:12 +0530

    completed first JPA project

--- testhibernate/src/main/java/testhibernate/EmployeeMain.java
+++ testhibernate/src/main/java/testhibernate/EmployeeMain.java
@@ -8,8 +8,16 @@ public class EmployeeMain {
 	public static void main(String[] args) {
 		EmployeeConfig employeeConfig = new EmployeeConfig();
 		
-		employeeConfig.insertRecord(2, "Sharath", 40, 90000);
+		// inserting a record
+//		employeeConfig.insertRecord(3, "Komal", 37, 50000);
 		
+		// read all records
+		employeeConfig.readAllRecords();
+		
+		// search record with empID
+		employeeConfig.searchRecordById(1);
+		
+			
 	}
 
 }
