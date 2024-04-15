adcce9a9d3b3becb54e101381a06f780a3d52949
shrivatsashetty <shrivatsa307@gmail.com>
2024-04-15 07:51:47 +0530

    prctice project to learn REST API

---
+++ restpractice/src/test/java/com/practice/restpractice/RestpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.restpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
